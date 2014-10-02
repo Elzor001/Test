2132e01df472b718613a4abead795f4d4de723c5
unknown <n015219c@k106-20.student.staffs.ac.uk>
2014-10-02 12:33:13 +0100

    Added player class

---
+++ Myfirstrepotest/Myfirstrepotest/Player.cs
@@ -0,0 +1,34 @@
+using System;
+
+
+
+
+namespace Myfirstrepotest
+{
+	
+	public class Player
+	{
+		
+		int health = 5;
+		float x = 0;
+		float y = 0;
+		float score = 0;
+		Boolean collide = false;
+		
+		
+		public Player ()
+		{
+			
+			
+		}
+		private Collision
+		{
+			
+			
+			
+			return Boolean = collide;
+		}
+		
+	}
+}
+
